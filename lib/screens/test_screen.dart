c7ce5d1fdb6bf34fcefdee7641edacdc3aed9891
Micahel-Data-Science-spec <mykuzmyn@gmail.com>
2023-01-22 15:51:41 +0200

    further tree rework

---
+++ lib/screens/test_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TestScreen extends StatelessWidget {
+  static const String routeName = "/test";
+
+  const TestScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
